56f6310d0ac4eb0e90bd09c7cc076387dba4be23
juliannah5203 <57543226+juliannah5203@users.noreply.github.com>
2024-04-12 18:19:59 -0700

    add additional tutorial levels, add tile prefabs for bomb, number1 & 2 &3, loadscene() added

--- Assets/Scripts/endmenue.cs
+++ Assets/Scripts/endmenue.cs
@@ -16,28 +16,38 @@ public class endmenue : MonoBehaviour
         Time.timeScale = 1.0f;
     }
 
-   public void loadtutorial(){
+   public void loadtutorial1(){
         SceneManager.LoadSceneAsync(2);
         Time.timeScale = 1.0f;
     }
 
+    public void loadtutorial2(){
+        SceneManager.LoadScene("Tutorial1-2");
+        Time.timeScale = 1.0f;
+    }
+    public void loadtutorial3(){
+        SceneManager.LoadScene("Tutorial1-3");
+        Time.timeScale = 1.0f;
+    }
+
     public void loadlevel1(){
-        SceneManager.LoadSceneAsync(3);
-        Time.timeScale = 1.0f;
-    }
-
-    public void loadlevel2(){
-        SceneManager.LoadSceneAsync(4);
-        Time.timeScale = 1.0f;
-    }
-
-    public void loadlevel3(){
         SceneManager.LoadSceneAsync(5);
         Time.timeScale = 1.0f;
     }
 
-    public void loadlevel4(){
+    public void loadlevel2(){
         SceneManager.LoadSceneAsync(6);
         Time.timeScale = 1.0f;
     }
+
+    public void loadlevel3(){
+        SceneManager.LoadSceneAsync(7);
+        Time.timeScale = 1.0f;
+    }
+
+    public void loadlevel4(){
+        SceneManager.LoadSceneAsync(8);
+        Time.timeScale = 1.0f;
+    }
+
 }
