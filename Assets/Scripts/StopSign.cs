d451de9b7b9f3e988bfa0e5c847adfcd6d59c0d7
SpikeFu2001 <meiwenfu@usc.edu>
2024-04-22 22:39:18 -0700

    updated tutorial 2

--- Assets/Scripts/StopSign.cs
+++ Assets/Scripts/StopSign.cs
@@ -5,55 +5,27 @@ using UnityEngine;
 public class StopSign : MonoBehaviour
 {
 
-    public GameObject bomb1;
-    public GameObject bomb2;
-    public GameObject bomb3;
-    public GameObject bomb4;
-    public GameObject bomb5;
-    [SerializeField] bool bomb1_Hidden;
-    [SerializeField] bool bomb2_Hidden;
-    [SerializeField] bool bomb3_Hidden;
-    [SerializeField] bool bomb4_Hidden;
-    [SerializeField] bool bomb5_Hidden;
+    public GameObject bomb;
+    public bool bomb_Hidden;
 
     // Start is called before the first frame update
     void Start()
     {
-        bomb1_Hidden = false;
-        bomb2_Hidden = false;
-        bomb3_Hidden = false;
-        bomb4_Hidden = false;
-        bomb5_Hidden = false;
+        bomb_Hidden = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (bomb1 != null)
+        if (bomb != null)
         {
-            bomb1_Hidden = bomb1.GetComponent<TileInteractor>().hidden;
-        }
-        if (bomb2 != null)
-        {
-            bomb2_Hidden = bomb2.GetComponent<TileInteractor>().hidden;
-        }
-        if (bomb3 != null)
-        {
-            bomb3_Hidden = bomb3.GetComponent<TileInteractor>().hidden;
-        }
-        if (bomb4 != null)
-        {
-            bomb4_Hidden = bomb4.GetComponent<TileInteractor>().hidden;
-        }
-        if (bomb5 != null)
-        {
-            bomb5_Hidden = bomb5.GetComponent<TileInteractor>().hidden;
+            bomb_Hidden = bomb.GetComponent<TileInteractor>().hidden;
         }
 
-            if (bomb1_Hidden && bomb2_Hidden && bomb3_Hidden && bomb4_Hidden && bomb5_Hidden)
-            {
-                this.gameObject.SetActive(false);
-            }
-        
+        if (bomb_Hidden)
+        {
+            this.gameObject.SetActive(false);
+        }
+
     }
 }
